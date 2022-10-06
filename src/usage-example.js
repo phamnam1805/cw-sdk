a06beaec40ba84878b291262218024ea7c03b69b
nam <nam.pv1805@gmail.com>
2022-10-06 13:29:50 +0700

    update

--- src/usage-example.js
+++ src/usage-example.js
@@ -1,22 +1,20 @@
-const { toBase64, parseMsg, Cosm } = require("./lib/cw-lib");
-const {malagaChain} = require("./lib/chain");
-const {instantiateCw20} = require("./instantiate-template");
-const {executeMint} = require("./execute-template");
-const {queryBalance} = require("./query-template")
+const { Cosm } = require("./lib/cw-lib");
+const { malagaChain } = require("./lib/chain");
+const { instantiateCw20 } = require("./instantiate-template");
+const { executeMint } = require("./execute-template");
+const { queryBalance } = require("./query-template")
 require("dotenv").config();
 const MNEMONIC1 = process.env.MNEMONIC1;
 async function main() {
     let cosm = await Cosm.init(malagaChain, MNEMONIC1);
     // deploy
-    console.log(await cosm.instantiateContract(instantiateCw20, 300000));
+    // console.log(await cosm.instantiateContract(instantiateCw20, 300000));
 
     // execute
-    // let msg = cosm.parseExecuteObject(executeMint);
-    // console.log(await cosm.execute(msg));
+    // console.log(await cosm.execute(executeMint));
 
-    // // query
-    // let msg = cosm.parseQueryObject(queryBalance);
-    // console.log(await cosm.query(msg));
+    // query
+    console.log(await cosm.query(queryBalance));
 
 }
 main();
